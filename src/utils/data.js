d1d881a600c612e43df61df385ae0fd062b411b4
Restyled.io <commits@restyled.io>
2023-01-27 12:16:36 +0000

    Restyled by clang-format

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,29 +1,29 @@
 export const plansData = [
   {
-    id: 'price_1MUpmZLWaDeJF2fyYaVheukH',
-    title: 'Basic',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
-    duration: 1,
-    price: 49,
-    durationText: 'mo',
+    id : 'price_1MUpmZLWaDeJF2fyYaVheukH',
+    title : 'Basic',
+    description :
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
+    duration : 1,
+    price : 49,
+    durationText : 'mo',
   },
   {
-    id: 'price_1MUpmzLWaDeJF2fywWAkqeKO',
-    title: 'Gold',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
-    price: 200,
-    duration: 6,
-    durationText: 'mo',
+    id : 'price_1MUpmzLWaDeJF2fywWAkqeKO',
+    title : 'Gold',
+    description :
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
+    price : 200,
+    duration : 6,
+    durationText : 'mo',
   },
   {
-    id: 'price_1MUpnELWaDeJF2fy2ngkYd8H',
-    title: 'Premium',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
-    price: 350,
-    duration: 1,
-    durationText: 'year',
+    id : 'price_1MUpnELWaDeJF2fy2ngkYd8H',
+    title : 'Premium',
+    description :
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia',
+    price : 350,
+    duration : 1,
+    durationText : 'year',
   },
 ];
