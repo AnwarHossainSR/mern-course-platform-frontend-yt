57bbfbd12476ca5121aa1e253b2f2e567ec2615a
Restyled.io <commits@restyled.io>
2023-01-31 10:49:35 +0000

    Restyled by clang-format

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,12 +1,9 @@
-import { useLocation } from 'react-router-dom';
-export const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
+import {useLocation} from 'react-router-dom';
+export const useQuery =
+    () => { return new URLSearchParams(useLocation().search);};
 
 export const getQueryUrl = (params) => {
-  return Object.keys(params)
-    .map((key) => `${key}=${params[key]}`)
-    .join('&');
+  return Object.keys(params).map((key) => `${key}=${params[key]}`).join('&');
 };
 
 export const getCourseFromId = (courses, id) => {
